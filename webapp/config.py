9b1f6dfe83a6808c4daa76e814f8aa03d24cd1b8
Talgat <valiullin.talgat@gmail.com>
2020-01-12 19:04:15 +0300

    First commit

---
+++ webapp/config.py
@@ -0,0 +1,4 @@
+WEATHER_DEFAULT_CITY = "Kazan,Russia"
+WEATHER_API_KEY = "a2186766ba0d4e1d918161740191712"
+WEATHER_URL ='http://api.worldweatheronline.com/premium/v1/weather.ashx'
+SQLALCHEMY_DATABASE_URI
